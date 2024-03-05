b8e2081f94a890f136a02fd8463005780d9c00f2
sparrowsl <benthorpe.dev@gmail.com>
2024-03-05 15:14:46 +0000

    feat: add base route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+// Return information about the API, including current version
+// and operating environment - dev, prod or staging
+func (app *application) checkHealth(writer http.ResponseWriter, request *http.Request) {
+
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 5000, "API Server Port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|staging|prod)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/v1/healthcheck", app.checkHealth)
+
+	server := &http.Server{
+		Handler:      mux,
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+	}
+
+	fmt.Printf("starting %s server on %s\n", cfg.env, server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		logger.Fatal(err)
+	}
 }

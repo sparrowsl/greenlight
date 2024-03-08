b06de4be9120dfa4f76f528b41772c20afaa24ad
sparrowsl <benthorpe.dev@gmail.com>
2024-03-08 12:27:50 +0000

    feat: customize struct with json tags

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64
-	Title     string
-	Runtime   int32
-	Year      int32
-	Genres    []string
-	Version   int32
-	CreatedAt time.Time
+	ID        int64     `json:"id"`
+	Title     string    `json:"title"`
+	Runtime   int32     `json:"runtime"`
+	Year      int32     `json:"year"`
+	Genres    []string  `json:"genres"`
+	Version   int32     `json:"version"`
+	CreatedAt time.Time `json:"created_at"`
 }

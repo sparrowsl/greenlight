67df5d458989bc4c1e9aa5db7abec32ea611a979
sparrowsl <benthorpe.dev@gmail.com>
2024-03-08 12:30:46 +0000

    feat: omit json tags with "omitempty" if they are empty

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,9 +5,9 @@ import "time"
 type Movie struct {
 	ID        int64     `json:"id"`
 	Title     string    `json:"title"`
-	Runtime   int32     `json:"runtime"`
-	Year      int32     `json:"year"`
-	Genres    []string  `json:"genres"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Year      int32     `json:"year,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 	CreatedAt time.Time `json:"created_at"`
 }

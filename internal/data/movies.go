031bd9b1bfccd1859d9074be8bcf7759f058e37e
sparrowsl <benthorpe.dev@gmail.com>
2024-03-08 13:01:41 +0000

    refactor: customize the runtime

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,7 +5,7 @@ import "time"
 type Movie struct {
 	ID        int64     `json:"id"`
 	Title     string    `json:"title"`
-	Runtime   int32     `json:"runtime,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
 	Year      int32     `json:"year,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`

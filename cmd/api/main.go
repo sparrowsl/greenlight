9cdbd8b81001b87ab8609c753d9332894fce1375
sparrowsl <benthorpe.dev@gmail.com>
2024-03-11 07:43:21 +0000

    feat: connect to postgresql db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -26,19 +33,23 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 5000, "API Server Port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|staging|prod)")
-	flag.String("dsn", "postgres://greenlight:password@localhost/greenlight", "DSN for database")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://sparrow:password@localhost/greenlight", "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Printf("database connection pool established...")
 
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/v1/healthcheck", app.checkHealth)
 
 	server := &http.Server{
 		Handler:      app.routes(),
@@ -48,8 +59,24 @@ func main() {
 		WriteTimeout: time.Second * 30,
 	}
 
-	fmt.Printf("starting %s server on %s\n", cfg.env, server.Addr)
+	logger.Printf("starting %s server on %s\n", cfg.env, server.Addr)
 	if err := server.ListenAndServe(); err != nil {
 		logger.Fatal(err)
 	}
+}
+
+func openDB(conf config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", conf.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.22.0
 
 require github.com/go-chi/chi/v5 v5.0.12
 
-require github.com/lib/pq v1.10.9 // indirect
+require github.com/lib/pq v1.10.9

5b9c777a53f0372705275339342c4c345aa27fc9
sparrowsl <benthorpe.dev@gmail.com>
2024-03-13 11:37:35 +0000

    feat: add migrations script with goose

---
+++ migrations/20240312161012_create_movies_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS movies(
+  id bigserial PRIMARY KEY,
+  created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW(),
+  title TEXT NOT NULL, 
+  year integer NOT NULL,
+  runtime INTEGER NOT NULL,
+  genres text[] NOT NULL,
+  version INTEGER NOT NULL DEFAULT 1
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS movies;
+-- +goose StatementEnd

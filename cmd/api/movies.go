f38424dfcbbfaa1c1a5aba4027432cfa11032189
sparrowsl <benthorpe.dev@gmail.com>
2024-03-14 18:29:39 +0000

    refactor: change PUT method to PATCH for movie update

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -90,10 +90,10 @@ func (app *application) updateMovie(writer http.ResponseWriter, request *http.Re
 	}
 
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
 	}
 
 	if err = app.readJSON(writer, request, &input); err != nil {
@@ -101,10 +101,21 @@ func (app *application) updateMovie(writer http.ResponseWriter, request *http.Re
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	val := validator.New()
 	if data.ValidateMovie(val, movie); !val.Valid() {

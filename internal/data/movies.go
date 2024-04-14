78fc87490519d1fc3bf5fb6b3e1bb36a1fd70d5f
sparrowsl <benthorpe.dev@gmail.com>
2024-04-14 21:57:00 +0000

    add LIMIT & OFFSET to SQL query

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -57,12 +57,13 @@ func (m *MovieModel) GetAll(title string, genres []string, filters Filters) ([]M
                 FROM movies
                 WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
                 AND (genres @> $2 OR $2 = '{}')
-                ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+                ORDER BY %s %s, id ASC
+				LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, statement, title, pq.Array(genres))
+	rows, err := m.DB.QueryContext(ctx, statement, title, pq.Array(genres), filters.limit(), filters.offset())
 	if err != nil {
 		return nil, err
 	}

ef4bf04957059d98547dc7258b44ba4650389034
sparrowsl <benthorpe.dev@gmail.com>
2024-04-15 07:12:58 +0000

    make robust rate limiter for clients

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,15 +4,37 @@ import (
 	"net"
 	"net/http"
 	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
 	var (
 		mux     sync.Mutex
-		clients = make(map[string]*rate.Limiter)
+		clients = make(map[string]*client)
 	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mux.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > time.Minute*3 {
+					delete(clients, ip)
+				}
+			}
+
+			mux.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		ip, _, err := net.SplitHostPort(request.RemoteAddr)
@@ -24,10 +46,12 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		mux.Lock()
 
 		if _, exists := clients[ip]; !exists {
-			clients[ip] = rate.NewLimiter(2, 4)
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
 		}
 
-		if !clients[ip].Allow() {
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
 			mux.Unlock()
 			app.rateLimitExceededResponse(writer, request)
 			return

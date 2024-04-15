b211429767277073a3af488a98d085e26dcece1d
sparrowsl <benthorpe.dev@gmail.com>
2024-04-15 07:03:55 +0000

    add clients ip rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"net"
 	"net/http"
+	"sync"
 
 	"golang.org/x/time/rate"
 )
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	limiter := rate.NewLimiter(2, 4)
+	var (
+		mux     sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
 
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(request.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(writer, request, err)
+			return
+		}
+
+		mux.Lock()
+
+		if _, exists := clients[ip]; !exists {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		if !clients[ip].Allow() {
+			mux.Unlock()
 			app.rateLimitExceededResponse(writer, request)
 			return
 		}
+
+		mux.Unlock()
 
 		next.ServeHTTP(writer, request)
 	})

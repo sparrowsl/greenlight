a035804e70c2256c83c9f91a87136ac3fe9eb168
sparrowsl <benthorpe.dev@gmail.com>
2024-07-03 13:16:14 +0000

    feat: add generateToken function

---
+++ internal/data/tokens.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
+	"time"
+)
+
+const (
+	scope = "activation"
+)
+
+type Token struct {
+	PlainText string
+	Hash      []byte
+	UserID    int64
+	Expiry    time.Time
+	Scope     string
+}
+
+func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserID: userID,
+		Expiry: time.Now().Add(ttl),
+		Scope:  scope,
+	}
+
+	randomBytes := make([]byte, 16)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	token.PlainText = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
+
+	hash := sha256.Sum256([]byte(token.PlainText))
+	token.Hash = hash[:]
+
+	return token, nil
+}

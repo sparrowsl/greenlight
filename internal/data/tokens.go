792edc460376a1d5b9fc791b3c992ac53446d568
sparrowsl <benthorpe.dev@gmail.com>
2024-07-05 12:17:57 +0000

    feat: add helper functions to manage tokens

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,10 +1,14 @@
 package data
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/sha256"
+	"database/sql"
 	"encoding/base32"
 	"time"
+
+	"github.com/sparrowsl/greenlight/internal/validator"
 )
 
 const (
@@ -17,6 +21,47 @@ type Token struct {
 	UserID    int64
 	Expiry    time.Time
 	Scope     string
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token, err := generateToken(userID, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Insert(token)
+	return token, err
+}
+
+func (m TokenModel) Insert(token *Token) error {
+	query := `INSERT INTO tokens (hash, user_id, expiry, scope)
+			VALUES ($1, $2, $3, $4)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, token.Hash, token.UserID, token.Expiry, token.Scope)
+	return err
+}
+
+func (m TokenModel) DeleteAllForUser(scope string, user User) error {
+	query := `DELETE FROM tokens
+			WHERE scope = $1 AND user_id = $2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, scope, user.ID)
+	return err
+}
+
+func ValidateTokenPlainText(v *validator.Validator, tokenPlainText string) {
+	v.Check(tokenPlainText != "", "token", "must be provided")
+	v.Check(len(tokenPlainText) == 26, "token", "must be 26 bytes long")
 }
 
 func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
